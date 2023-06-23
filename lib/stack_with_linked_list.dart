6fbc2b0d8afb528053fa9807162af644e73e49bb
mohanadhabib <mohaned14112001@gmail.com>
2023-06-23 19:10:31 +0300

    second Commit

---
+++ lib/stack_with_linked_list.dart
@@ -0,0 +1,49 @@
+class Node<T>{
+  late T value;
+  late Node<T>? next;
+}
+class Stack<T>{
+  Node<T>? top;
+  Stack(){
+    top = null;
+  }
+  bool isEmpty(){
+    return top == null;
+  }
+  void push(T element){
+    Node<T>? newNode = new Node<T>();
+    newNode.value = element;
+    newNode.next = top;
+    top = newNode;
+  }
+  void pop(){
+    if(isEmpty()){
+      print("Stack is Empty");
+    }
+    else{
+      top = top!.next;
+    }
+  }
+  T getTop(){
+    return top!.value;
+  }
+  void printStack(){
+    Node<T>? temp = top;
+    print('[ ');
+    while(temp!=null){
+      print('${temp.value} ');
+      temp = temp.next;
+    }
+    print(']');
+  }
+}
+void main(){
+  Stack<int> s = new Stack<int>();
+  s.push(1);
+  s.push(2);
+  s.push(3);
+  s.push(4);
+  s.pop();
+  s.printStack();
+  print('Top : ${s.getTop()}');
+}
